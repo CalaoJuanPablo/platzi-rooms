81df07bf4354f67bf8d1ac9466185fbda913eb8e
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-06-28 09:26:34 -0500

    create room logic in state

--- src/views/CreateHousePage.vue
+++ src/views/CreateHousePage.vue
@@ -20,18 +20,18 @@
         <form>
           <div class="mb-4">
             <label for="room-title" class="input__label">Title</label>
-            <input id="room-title" class="input__field" type="text" placeholder="Bruce Wayne">
+            <input v-model="publication.title" id="room-title" class="input__field" type="text" placeholder="Bruce Wayne">
           </div>
           <div class="mb-4">
             <label for="room-description" class="input__label">Description</label>
-            <textarea id="room-description" class="input__field" rows="10" placeholder="Bruce Wayne"></textarea>
+            <textarea v-model="publication.description" id="room-description" class="input__field" rows="10" placeholder="Bruce Wayne"></textarea>
           </div>
           <div class="mb-4">
             <label for="room-image" class="input__label">Featured image</label>
-            <input id="room-image" class="input__field" type="text" placeholder="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiwkbXPiorjAhWy1lkKHXkkCcAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.the-connaught.co.uk%2Frooms-and-suites%2F&psig=AOvVaw1JQ4TMoQTPrTpy20OKULhC&ust=1561739356498937">
+            <input v-model="publication.featuredImage" id="room-image" class="input__field" type="text" placeholder="https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiwkbXPiorjAhWy1lkKHXkkCcAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.the-connaught.co.uk%2Frooms-and-suites%2F&psig=AOvVaw1JQ4TMoQTPrTpy20OKULhC&ust=1561739356498937">
           </div>
           <div class="mb-4 text-right">
-            <button class="w-full bg-yellow-dark text-yellow-darker font-semibold py-3 px-6 rounded">Publish</button>
+            <button @click.prevent="save" class="w-full bg-yellow-dark text-yellow-darker font-semibold py-3 px-6 rounded">Publish</button>
           </div>
         </form>
       </div>
@@ -45,6 +45,27 @@ export default {
   name: 'CreateHousePage',
   components: {
     PageLayout
+  },
+  data () {
+    return {
+      publication: {
+        title: '',
+        description: '',
+        featuredImage: ''
+      }
+    }
+  },
+  methods: {
+    save () {
+      const { title, description, featuredImage } = this.publication;
+      const room = {
+        title,
+        description,
+        feature_image: featuredImage,
+        publishedAt: Date.now(),
+      }
+      this.$store.dispatch('CREATE_ROOM', room);
+    }
   }
 }
 </script>
