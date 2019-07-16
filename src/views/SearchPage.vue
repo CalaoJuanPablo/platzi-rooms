0af1298b16c75400dbdaaed6bf0ac2000595d9b2
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-07-16 09:15:02 -0500

    links with vue router

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -25,11 +25,6 @@
             :room="room">
           </house-card>
         </div>
-        <div class="text-center">
-          <a
-            class="py-3 px-12 bg-yellow-dark no-underline text-yellow-darker text-lg rounded"
-            href="#">Show all</a>
-        </div>
       </div>
     </section>
   </page-layout>
